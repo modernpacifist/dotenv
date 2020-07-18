bf0154addfe9741ab6bb6366439f8ba8b60118e4
modernpacifist <modernpacifist@gmail.com>
2020-07-19 01:44:42 +0300

    created automated file for downloads for this config

---
+++ i3config/autoinstall_soft.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# for background wallpaper
+sudo apt install feh
+
+# for graphical file explorer
+sudo apt install caja
+
+# for hidden cursor
+sudo apt install unclutter
+
+# for volume visualization
+sudo apt install volumeicon-alsa
+
+# for screenshots
+sudo apt install scrot
+# screenshots into the clipboard instantly
+sudo apt install xclip
+
+# for swap of escape with caps-lock
+sudo apt install setxkbmap
