a017cdf752aa834913d1d144fe249afe6d8606b6
modernpacifist <modernpacifist@gmail.com>
2021-04-07 07:18:22 +0300

    07/04/2021-07:18:22 automated backup commit

--- install.sh
+++ install.sh
@@ -1,37 +1,26 @@
 #!/bin/env bash
 if [[ $1 ]];
 then
-    dotenv_dir=$(pwd)
-
     # i3 configs setup done
-    cd ./i3config
-    cp config /home/$1/.config/i3/
-    cp i3-keyboard-layout /bin/
-    cp i3nonprimary_status.conf /etc/
-    cp i3status.conf /etc/
-    cd $dotenv_dir
+    cp ./i3config/config /home/$1/.config/i3/
+    cp ./i3config/i3-keyboard-layout /bin/
+    cp ./i3config/i3nonprimary_status.conf /etc/
+    cp ./i3config/i3status.conf /etc/
 
     # dotfiles setup done
-    cd ./dotfiles
-    cp .bash_aliases /home/$1/ && cp .bash_aliases ~/
-    cp .bashrc /home/$1/ && cp .bashrc ~/
-    cp .gdbinit /home/$1/ && cp .gdbinit ~/
-    cp .inputrc /home/$1/ && cp .inputrc ~/
-    cp .vimrc /home/$1/ && cp .vimrc ~/
-    cd $dotenv_dir
+    cp ./dotfiles/.bash_aliases /home/$1/ && cp .bash_aliases ~/
+    cp ./dotfiles/.bashrc /home/$1/ && cp .bashrc ~/
+    cp ./dotfiles/.gdbinit /home/$1/ && cp .gdbinit ~/
+    cp ./dotfiles/.inputrc /home/$1/ && cp .inputrc ~/
+    cp ./dotfiles/.vimrc /home/$1/ && cp .vimrc ~/
 
     # custom-binaries setup done
-    cd ./custom-binaries
-    cp ./* /bin/
-    cd $dotenv_dir
+    cp ./custom-binaries/* /bin/
 
     # alacritty config
     alacritty_config_dir=/home/$1/.config/alacritty
     if [[ -d $alacritty_config_dir ]]; then
-        cd $alacritty_config_dir
-        cd ./alacritty
-        cp ./alacritty.yml $alacritty_config_dir
-        cd $dotenv_dir
+        cp ./alacritty/alacritty.yml $alacritty_config_dir
     fi
 
     exit 0
