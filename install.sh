cd32d5aa949566e1a12e449295b6e62f027fa02c
modernpacifist <modernpacifist@gmail.com>
2021-04-10 22:23:49 +0300

    10/04/2021-22:23:49 automated backup commit

--- install.sh
+++ install.sh
@@ -1,30 +1,35 @@
 #!/bin/env bash
-if [[ $1 ]];
-then
-    # i3 configs setup done
-    cp ./i3config/config /home/$1/.config/i3/
-    cp ./i3config/i3-keyboard-layout /bin/
-    cp ./i3config/i3nonprimary_status.conf /etc/
-    cp ./i3config/i3status.conf /etc/
-
-    # dotfiles setup done
-    cp ./dotfiles/.bash_aliases /home/$1/ && cp ./dotfiles/.bash_aliases ~/
-    cp ./dotfiles/.bashrc /home/$1/ && cp ./dotfiles/.bashrc ~/
-    cp ./dotfiles/.gdbinit /home/$1/ && cp ./dotfiles/.gdbinit ~/
-    cp ./dotfiles/.inputrc /home/$1/ && cp ./dotfiles/.inputrc ~/
-    cp ./dotfiles/.vimrc /home/$1/ && cp ./dotfiles/.vimrc ~/
-
-    # custom-binaries setup done
-    cp ./custom-binaries/* /bin/
-
-    # alacritty config
-    alacritty_config_dir=/home/$1/.config/alacritty
-    if [[ -d $alacritty_config_dir ]]; then
-        cp ./alacritty/alacritty.yml $alacritty_config_dir
-    fi
-
-    exit 0
-else
-    echo 'Specify username in $1'
+if [[ $UID != 0 ]]; then
+    echo "You need to be root to install all features" >&2
     exit 1
 fi
+
+for file in $(ls -A custom-binaries); do
+    cp $file /bin/
+done
+
+available_users=$(ls /home)
+for username in $available_users
+do
+    if [[ -d /home/$username/.config/i3/ ]]; then
+        cp ./i3config/config /home/$username/.config/i3/
+        cp ./i3config/i3-keyboard-layout /bin/
+        cp ./i3config/i3nonprimary_status.conf /etc/
+        cp ./i3config/i3status.conf /etc/
+    fi
+
+    if [[ -d /home/$username/ ]]; then
+        files=$(ls -A ./dotfiles/)
+        for file in $files
+        do
+            cp ./dotfiles/$file /home/$username/
+            cp ./dotfiles/$file ~/
+        done
+    fi
+
+    if [[ -d /home/$username/.config/alacritty ]]; then
+        cp ./alacritty/alacritty.yml /home/$username/.config/alacritty
+    fi
+done
+
+exit 0
