53f69789165f287404e1223b2902a983d78dd8d8
modernpacifist <modernpacifist@gmail.com>
2021-05-22 18:00:01 +0300

    22/05/2021-18:00:01 automated backup commit

--- fish/config.fish
+++ fish/config.fish
@@ -14,6 +14,10 @@ if type -q exa
   alias ll "exa -l -a -g --tree --level=2 --icons --color=never"
 end
 
+function fish_right_prompt -d "Write out the right prompt"
+    date "+%T"
+end
+
 function fish_user_key_bindings
   bind \cr hstr
   bind \cc _cpwd
