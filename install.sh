7ca4b0008225f7cdc08cff83df36e1469ddcbb87
modernpacifist <modernpacifist@gmail.com>
2021-06-24 04:54:59 +0300

    simplified install.sh a lot

--- install.sh
+++ install.sh
@@ -1,56 +1,42 @@
 #!/bin/env bash
-if [[ $UID != 0 ]]; then
-    echo "You need to be root to install all features" >&2
-    exit 1
+
+function root_setup {
+    cp ./binfiles/* /bin/
+
+    if [[ -d /etc/bash_completion.d/ ]]; then
+        cp ./bash-completions/* /etc/bash_completion.d/
+    fi
+
+    if [[ -d /etc/fish/completions/ ]]; then
+        cp ./fish-completions/* /etc/fish/completions/
+    fi
+}
+
+if [[ $EUID == 0 ]]; then
+    root_setup
 fi
 
-# Installation of binaries
-for bin_file in $(ls -A ./custom-binaries/); do
-    cp ./custom-binaries/$bin_file /bin/ || echo "./custom-binaries/${bin_file} was not copied"
-done
+# dotfiles setup
+cp ./dotfiles/* $HOME/
 
-# bash completions for binaries
-for bash_comp_file in $(ls -A ./bash-completions/); do
-    cp ./bash-completions/$bash_comp_file /etc/bash_completion.d/$bash_comp_file || echo "./bash-completions/${bash_comp_file} was not copied"
-done
+# i3wm setup
+if [[ -d $HOME/.config/i3/ ]]; then
+    cp ./i3config/* $HOME/.config/i3/
+fi
 
-# Installation of dotfiles for root
-for dot_file in $(ls -A ./dotfiles/); do
-    cp ./dotfiles/$dot_file ~/ || echo "./dotfiles/${dot_file} was not copied"
-done
+# alacritty setup
+if [[ -d $HOME/.config/alacritty ]]; then
+    cp ./alacritty/alacritty.yml $HOME/.config/alacritty/
+fi
 
-available_users=$(ls -A /home)
-for username in $available_users; do
-    if [[ -d /home/$username/.config/i3/ ]]; then
-        cp ./i3config/config /home/$username/.config/i3/
-        cp ./i3config/i3-keyboard-layout /bin/
-        cp ./i3config/i3nonprimary_status.conf /etc/
-        cp ./i3config/i3status.conf /etc/
-    fi
+# kitty setup
+if [[ -d $HOME/.config/kitty ]]; then
+    cp ./kitty/kitty.conf $HOME/.config/kitty/
+fi
 
-    if [[ -d /home/$username/ ]]; then
-        for dot_file in $(ls -A ./dotfiles/); do
-            cp ./dotfiles/$dot_file /home/$username/$dot_file
-        done
-    fi
-
-    if [[ -d /home/$username/.config/alacritty ]]; then
-        cp ./alacritty/alacritty.yml /home/$username/.config/alacritty
-    fi
-
-    if [[ -d /home/$username/.config/fish ]]; then
-        for fish_file in $(ls -A ./fish/); do
-            cp ./fish/$fish_file /home/$username/.config/fish/$fish_file
-        done
-        # fish completions
-        for fish_comp_file in $(ls -A ./fish-completions/); do
-            cp ./fish-completions/$fish_comp_file /etc/fish/completions/$fish_comp_file || echo "./fish-completions/${fish_comp_file} was not copied"
-        done
-    fi
-
-    if [[ -d /home/$username/.config/kitty ]]; then
-        cp ./kitty/kitty.conf /home/$username/.config/kitty/
-    fi
-done
+# fish setup
+if [[ -d $HOME/.config/fish ]]; then
+    cp ./fish/* $HOME/.config/fish/
+fi
 
 exit 0
