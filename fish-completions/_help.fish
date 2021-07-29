544c217279110f5c9bd8ee083eeb0c5fc9ea6861
modernpacifist <modernpacifist@gmail.com>
2021-07-29 17:03:48 +0300

    29/07/2021-17:03:48 automated backup commit

--- fish-completions/_help.fish
+++ fish-completions/_help.fish
@@ -1,1 +1,5 @@
-complete -f -c _help -a "git gdb syscalls bash-scripting jupyter-lab"
+complete -f -c _help -n __fish_use_subcommand -a git
+complete -f -c _help -n __fish_use_subcommand -a gdb
+complete -f -c _help -n __fish_use_subcommand -a syscalls
+complete -f -c _help -n __fish_use_subcommand -a bash-scripting
+complete -f -c _help -n __fish_use_subcommand -a jupyter-lab
--- fish-completions/_tor.fish
+++ fish-completions/_tor.fish
@@ -1,1 +1,4 @@
-complete -f -c _tor -a "start restart status stop"
+complete -f -c _tor -n __fish_use_subcommand -a start -d 'systemctl start tor.service'
+complete -f -c _tor -n __fish_use_subcommand -a restart -d 'systemctl restart tor.service'
+complete -f -c _tor -n __fish_use_subcommand -a status -d 'systemctl status tor.service'
+complete -f -c _tor -n __fish_use_subcommand -a stop -d 'systemctl stop tor.service'
