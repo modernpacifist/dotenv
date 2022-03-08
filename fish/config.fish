7acf8caad51d9398d0f5e8b03bff4c7c7b122c3f
modernpacifist <modernpacifist@gmail.com>
2022-03-09 02:46:01 +0300

    09/03/2022-02:46:01 automated backup commit

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,8 @@
 set fish_greeting
 set -x PYTHONSTARTUP $HOME/.pythonstartup
 set -x EDITOR vim
+set -x PATH $PATH /sbin/
+set -x PATH $PATH /usr/local/go/bin
 
 abbr -a -- - 'cd -'
 alias .='pwd'
@@ -11,8 +13,8 @@ alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 
 if type -q exa
-  alias l "exa -l -a --icons --color=never"
-  alias ll "exa -l -a -g --tree --level=2 --icons --color=never"
+    alias l "exa -l -a --icons --color=never"
+    alias ll "exa -l -a -g --tree --level=2 --icons --color=never"
 end
 
 function fish_right_prompt -d "Write out the right prompt"
@@ -20,19 +22,19 @@ function fish_right_prompt -d "Write out the right prompt"
 end
 
 function peco_select_history
-  if test (count $argv) = 0
-    set peco_flags --layout=bottom-up
-  else
-    set peco_flags --layout=bottom-up --query "$argv"
-  end
+    if test (count $argv) = 0
+        set peco_flags --layout=bottom-up
+    else
+        set peco_flags --layout=bottom-up --query "$argv"
+    end
 
-  history|peco $peco_flags|read foo
+    history|peco $peco_flags|read foo
 
-  if [ $foo ]
-    commandline $foo
-  else
-    commandline ''
-  end
+    if [ $foo ]
+        commandline $foo
+    else
+        commandline ''
+    end
 end
 
 function fish_user_key_bindings
