f573c1997ecc88fd99489d5055e008f4c35a91d1
modernpacifist <modernpacifist@gmail.com>
2023-11-16 14:55:03 +0300

    proelium-exercitum:16/11/2023-14:55:03 automated backup commit

--- i3/uptime.sh
+++ i3/uptime.sh
@@ -5,8 +5,8 @@ set -e
 while :
 do
     read line
-    value=$(awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime)
-    echo $value > /tmp/i3statusuptime
+    #value=$(awk '{printf("%d:%02d:%02d:%02d\n",($1/60/60/24),($1/60/60%24),($1/60%60),($1%60))}' /proc/uptime)
+    echo $(uptime -p) > /tmp/i3statusuptime
     block='{"full_text":""}'
     echo "${line/\[\{/\[$block,\{}" || exit 1
 done
